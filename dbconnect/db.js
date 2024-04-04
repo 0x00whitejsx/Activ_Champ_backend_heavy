f45c4b7ffd1df47cf68aef798166395231f4203b
whitejsx <0x00whitejsx@gmail.com>
2024-04-04 11:15:24 +0100

    all file added

--- dbconnect/db.js
+++ dbconnect/db.js
@@ -1,11 +1,16 @@
-const mongoose  = require('mongoose')
-require('dotenv').config()
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Database connected");
+    } catch (error) {
+        console.error("Error connecting to database:", error);
+    }
+}
 
-
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => {
-    console.log("Databse connected")
-}).catch((err) => {
-    console.log(err)
-})
+connectToDatabase();
