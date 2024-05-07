5d8f002202c238168b95ac1b84943f4ebbfa4a0b
0x00whitejsx <0x00whitejsx@gmail.com>
2024-05-07 13:31:47 +0100

    add the table link

--- server.js
+++ server.js
@@ -19,10 +19,54 @@ app.use(cookiePerser())
 
 app.get('/', (req, res) => {
     res.send( `<center>
+    <style>
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    th, td {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+    th {
+        background-color: #f2f2f2;
+    }
+</style>
                 <h1>Active Champ API </h1> 
                 <br /><a href='https://documenter.getpostman.com/view/19754715/2sA3Bj7ZDj'>here is the link</a>
                 <table>
-                </table>
+                <tr>
+                  <th>API</th>
+                  <th>Method</th>
+                  <th>Description</th>
+                </tr>
+                <tr>
+                  <td>users</td>
+                  <td>GET</td>
+                  <td>Retrieve list of users</td>
+                </tr>
+                <tr>
+                  <td>users/:id</td>
+                  <td>GET</td>
+                  <td>Retrieve details of a specific user</td>
+                </tr>
+                <tr>
+                  <td>users</td>
+                  <td>POST</td>
+                  <td>Create a new user</td>
+                </tr>
+                <tr>
+                  <td>users/:id</td>
+                  <td>PUT</td>
+                  <td>Update details of a specific user</td>
+                </tr>
+                <tr>
+                  <td>users/:id</td>
+                  <td>DELETE</td>
+                  <td>Delete a specific user</td>
+                </tr>
+              </table>
                 <center>`)
 })
 
